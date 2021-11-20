559077c35ac9db58da7d14df0a7ff029848efc98
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-20 17:10:55 +0700

    add: endpoints setup

---
+++ routes/index.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rizadwiandhika/miniproject-backend-alterra/factory"
+)
+
+func Setup() *echo.Echo {
+	e := echo.New()
+
+	presenter := factory.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	SetupArticleRoutes(e, presenter)
+
+	return e
+}
