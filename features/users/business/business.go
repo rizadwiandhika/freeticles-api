5a9bfb64734e48ba50df6dc2f29b6529e24e1853
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-20 22:58:53 +0700

    add: users business functionalities

--- features/users/business/business.go
+++ features/users/business/business.go
@@ -15,3 +15,27 @@ func NewBusiness(data users.IData) *userBusiness {
 func (ub *userBusiness) FindUserById(id uint) (users.UserCore, error) {
 	return ub.userData.SelectUserById(id)
 }
+
+func (ub *userBusiness) FindUsers() ([]users.UserCore, error) {
+	return ub.userData.SelectUsers()
+}
+
+func (ub *userBusiness) FindUserByUsername(username string) (users.UserCore, error) {
+	return ub.userData.SelectUserByUsername(username)
+}
+
+func (ub *userBusiness) FindUserByEmail(email string) (users.UserCore, error) {
+	return ub.userData.SelectUserByEmail(email)
+}
+
+func (ub *userBusiness) CreateUser(user users.UserCore) (users.UserCore, error) {
+	return ub.userData.InsertUser(user)
+}
+
+func (ub *userBusiness) EditUser(user users.UserCore) (users.UserCore, error) {
+	return ub.userData.UpdateUser(user)
+}
+
+func (ub *userBusiness) RemoveUser(username string) error {
+	return ub.userData.DeleteUser(username)
+}
