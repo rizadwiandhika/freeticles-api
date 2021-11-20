3ec69a2e2256f0394c6f79d844ffdddddf10545f
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-20 16:35:47 +0700

    add: articles presentation

---
+++ features/articles/presentation/presentation.go
@@ -0,0 +1,35 @@
+package presentation
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rizadwiandhika/miniproject-backend-alterra/features/articles"
+)
+
+type any interface{}
+type json map[string]any
+type list []any
+
+type ArticlePresentation struct {
+	articleBusiness articles.IBusiness
+}
+
+func NewPresentation(articleBusiness articles.IBusiness) *ArticlePresentation {
+	return &ArticlePresentation{articleBusiness}
+}
+
+func (ap ArticlePresentation) GetDetailArticle(c echo.Context) error {
+	var id uint
+	echo.PathParamsBinder(c).Uint("id", &id)
+
+	articles, err := ap.articleBusiness.FindArticleById(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, json{
+			"message": "Could not get article",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, json{"articles": articles})
+}
