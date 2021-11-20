40b0ed75ca12997f56f6b47e6a0d3a160ad13d05
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-20 17:12:10 +0700

    feat: get detial article

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/rizadwiandhika/miniproject-backend-alterra/config"
+	"github.com/rizadwiandhika/miniproject-backend-alterra/migration"
+	"github.com/rizadwiandhika/miniproject-backend-alterra/routes"
 )
 
 func main() {
 	config.LoadEnv()
 	config.ConnectDB()
+	migration.AutoMigrate()
 
-	// e := routes.Setup()
-	// log.Fatalln(e.Start(":8080"))
+	e := routes.Setup()
+	log.Fatalln(e.Start(":8080"))
 }
