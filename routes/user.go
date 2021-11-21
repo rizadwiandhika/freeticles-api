6c41373f6d4ed768d8368013321747499677894f
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-21 08:03:50 +0700

    feat: factory for users topic

---
+++ routes/user.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/rizadwiandhika/miniproject-backend-alterra/factory"
+)
+
+func SetupUserRoutes(e *echo.Echo, presenter *factory.Presenter) {
+	routes := e.Group("/users")
+
+	routes.GET("/:username", presenter.UserPresentation.GetDetailUser)
+	routes.DELETE("/:username", presenter.UserPresentation.DeleteUser)
+	routes.PUT("/:username", presenter.UserPresentation.PutEditUser)
+
+	routes.GET("", presenter.UserPresentation.GetUsers)
+}
