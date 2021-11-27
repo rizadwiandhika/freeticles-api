30836a486e804d2819115b1c63d4ced433a7ebc5
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-27 12:24:20 +0700

    add: reaction factory

--- factory/factory.go
+++ factory/factory.go
@@ -10,32 +10,41 @@ import (
 	authBusiness "github.com/rizadwiandhika/miniproject-backend-alterra/features/auth/business"
 	authPresentation "github.com/rizadwiandhika/miniproject-backend-alterra/features/auth/presentation"
 
+	reactionsBusiness "github.com/rizadwiandhika/miniproject-backend-alterra/features/reactions/business"
+	reactionsData "github.com/rizadwiandhika/miniproject-backend-alterra/features/reactions/data"
+	reactionsPresentation "github.com/rizadwiandhika/miniproject-backend-alterra/features/reactions/presentation"
+
 	usersBusiness "github.com/rizadwiandhika/miniproject-backend-alterra/features/users/business"
 	usersData "github.com/rizadwiandhika/miniproject-backend-alterra/features/users/data"
 	usersPresentation "github.com/rizadwiandhika/miniproject-backend-alterra/features/users/presentation"
 )
 
 type Presenter struct {
-	ArticlePresentation *articlesPresentation.ArticlePresentation
-	UserPresentation    *usersPresentation.UserPresentation
-	AuthPresentation    *authPresentation.AuthPresentation
+	ArticlePresentation  *articlesPresentation.ArticlePresentation
+	UserPresentation     *usersPresentation.UserPresentation
+	AuthPresentation     *authPresentation.AuthPresentation
+	ReactionPresentation *reactionsPresentation.ReactionPresentation
 }
 
 func New() *Presenter {
 	userData := usersData.NewMySQLRepository(config.DB)
 	articleData := articlesData.NewMySQLRepository(config.DB)
+	reactionData := reactionsData.NewMySQLRepository(config.DB)
 
 	userBusiness := usersBusiness.NewBusiness(userData)
 	articleBusiness := articlesBusiness.NewBusiness(articleData, userBusiness)
 	authBusiness := authBusiness.NewBusniness(userBusiness)
+	reactionBusiness := reactionsBusiness.NewBusiness(reactionData, userBusiness, articleBusiness)
 
 	userPresentation := usersPresentation.NewPresentation(userBusiness)
 	articlePresentation := articlesPresentation.NewPresentation(articleBusiness)
 	authPresentation := authPresentation.NewPresentation(authBusiness)
+	reactionPresentation := reactionsPresentation.NewPresentation(reactionBusiness)
 
 	return &Presenter{
-		ArticlePresentation: articlePresentation,
-		UserPresentation:    userPresentation,
-		AuthPresentation:    authPresentation,
+		ArticlePresentation:  articlePresentation,
+		UserPresentation:     userPresentation,
+		AuthPresentation:     authPresentation,
+		ReactionPresentation: reactionPresentation,
 	}
 }
