772b4adcd1b316f86afbc5ddaf10df7d8e5d0726
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-27 12:25:06 +0700

    update: migration add like, report_type, report models

--- migration/migrate.go
+++ migration/migrate.go
@@ -1,62 +1,87 @@
 package migration
 
 import (
+	"encoding/json"
+
 	"github.com/rizadwiandhika/miniproject-backend-alterra/config"
 	article "github.com/rizadwiandhika/miniproject-backend-alterra/features/articles/data"
+	reaction "github.com/rizadwiandhika/miniproject-backend-alterra/features/reactions/data"
 	user "github.com/rizadwiandhika/miniproject-backend-alterra/features/users/data"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func AutoMigrate() {
-	if err := config.DB.Exec("DROP TABLE IF EXISTS users").Error; err != nil {
+	if err := config.DB.Exec("DROP TABLE IF EXISTS tags").Error; err != nil {
 		panic(err)
 	}
-	if err := config.DB.Exec("DROP TABLE IF EXISTS tags").Error; err != nil {
+	if err := config.DB.Exec("DROP TABLE IF EXISTS comments").Error; err != nil {
+		panic(err)
+	}
+	if err := config.DB.Exec("DROP TABLE IF EXISTS likes").Error; err != nil {
+		panic(err)
+	}
+	if err := config.DB.Exec("DROP TABLE IF EXISTS reports").Error; err != nil {
+		panic(err)
+	}
+	if err := config.DB.Exec("DROP TABLE IF EXISTS report_types").Error; err != nil {
 		panic(err)
 	}
 	if err := config.DB.Exec("DROP TABLE IF EXISTS articles").Error; err != nil {
 		panic(err)
 	}
+	if err := config.DB.Exec("DROP TABLE IF EXISTS users").Error; err != nil {
+		panic(err)
+	}
 
-	err := config.DB.AutoMigrate(&user.User{}, &article.Article{}, &article.Tag{})
+	err := config.DB.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+		&article.Tag{},
+		&reaction.Comment{},
+		&reaction.Like{},
+		&reaction.ReportType{},
+		&reaction.Report{},
+	)
+
 	if err != nil {
 		panic(err)
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("riza123"), 14)
+	populateDBWithDummyData()
+}
+
+func populateDBWithDummyData() {
+	pw1, err := bcrypt.GenerateFromPassword([]byte("riza123"), 14)
+	if err != nil {
+		panic(err)
+	}
+	pw2, err := bcrypt.GenerateFromPassword([]byte("hernowo123"), 14)
+	if err != nil {
+		panic(err)
+	}
+	pw3, err := bcrypt.GenerateFromPassword([]byte("hammim123"), 14)
 	if err != nil {
 		panic(err)
 	}
 
 	usr1 := user.User{
 		Username: "riza.dwii",
-		Email:    "rizadwiandhika@mail.com",
-		Password: string(hashedPassword),
+		Email:    "riza@mail.com",
+		Role:     "admin",
+		Password: string(pw1),
 		Name:     "Riza Dwi Andhika",
 	}
 	usr2 := user.User{
 		Username: "hernowoari",
-		Email:    "owo@mail.com",
-		Password: string(hashedPassword),
+		Email:    "hernowo@mail.com",
+		Password: string(pw2),
 		Name:     "Hernowo Ari Sutanto",
 	}
-
-	tags := []article.Tag{
-		{ArticleID: 1, Tag: "anime"},
-		{ArticleID: 1, Tag: "sport"},
-		{ArticleID: 1, Tag: "fun"},
-	}
-	arr := article.Article{
-		Title:    "Test Article",
-		Content:  `Some random content`,
-		AuthorID: 1,
-		Tags:     tags,
-	}
-	arr2 := article.Article{
-		Title:    "Another Article",
-		Content:  `Again... random content`,
-		AuthorID: 2,
-		Tags:     tags,
+	usr3 := user.User{
+		Username: "hammim",
+		Email:    "hammim@mail.com",
+		Password: string(pw3),
+		Name:     "Hammim Eka",
 	}
 
 	err = config.DB.Create(&usr1).Error
@@ -67,8 +92,33 @@ func AutoMigrate() {
 	if err != nil {
 		panic(err)
 	}
+	err = config.DB.Create(&usr3).Error
+	if err != nil {
+		panic(err)
+	}
 
-	err = config.DB.Create(&arr).Error
+	arr1 := article.Article{
+		Title:    "Test Article",
+		Content:  `Some random content`,
+		AuthorID: 1,
+		Tags: []article.Tag{
+			{Tag: "anime"},
+			{Tag: "sport"},
+			{Tag: "fun"},
+		},
+	}
+	arr2 := article.Article{
+		Title:    "Another Article",
+		Content:  `Again... random content`,
+		AuthorID: 2,
+		Tags: []article.Tag{
+			{Tag: "car"},
+			{Tag: "education"},
+			{Tag: "hobby"},
+		},
+	}
+
+	err = config.DB.Create(&arr1).Error
 	if err != nil {
 		panic(err)
 	}
@@ -77,4 +127,38 @@ func AutoMigrate() {
 		panic(err)
 	}
 
+	rt1 := reaction.ReportType{
+		Name:        "spam",
+		Description: "Article is considered as spam",
+	}
+	rt2 := reaction.ReportType{
+		Name:        "sexual content",
+		Description: "Article content may not appropriate for certain users",
+	}
+	rt3 := reaction.ReportType{
+		Name:        "just don't like",
+		Description: "Users somehow just don't like the article content",
+	}
+
+	err = config.DB.Create(&rt1).Error
+	if err != nil {
+		panic(err)
+	}
+	err = config.DB.Create(&rt2).Error
+	if err != nil {
+		panic(err)
+	}
+	err = config.DB.Create(&rt3).Error
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func outputAsJSON(v interface{}) string {
+	JSON, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return string(JSON)
 }
