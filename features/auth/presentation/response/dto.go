5831f45ee1c9e1cc15b36015f4532db7bc5fa4fe
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-27 07:10:38 +0700

    add: response dto in auth presentation

--- features/auth/presentation/response/dto.go
+++ features/auth/presentation/response/dto.go
@@ -1,1 +1,49 @@
 package response
+
+import "github.com/rizadwiandhika/miniproject-backend-alterra/features/auth"
+
+type UserRegisterSuccess struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+}
+
+type UserRegisterFailed struct {
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserLoginFailed struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func ToUserRegisterSuccess(u auth.UserCore) UserRegisterSuccess {
+	return UserRegisterSuccess{
+		ID:       u.ID,
+		Username: u.Username,
+		Name:     u.Name,
+		Email:    u.Email,
+	}
+}
+
+func ToUserRegisterFailed(u auth.UserCore) UserRegisterFailed {
+	return UserRegisterFailed{
+		Username: u.Username,
+		Name:     u.Name,
+		Email:    u.Email,
+		Password: u.Password,
+	}
+}
+
+func ToUserLoginFailed(u auth.UserCore) UserLoginFailed {
+	return UserLoginFailed{
+		Username: u.Username,
+		Email:    u.Email,
+		Password: u.Password,
+	}
+}
