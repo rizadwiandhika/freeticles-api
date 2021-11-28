f8a0d340bbbfddf41827af23995b4434b32bfbb6
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-28 18:39:40 +0700

    feat: setup routes for bookmarks

--- routes/user.go
+++ routes/user.go
@@ -10,13 +10,16 @@ func SetupUserRoutes(e *echo.Echo, presenter *factory.Presenter) {
 	routes := e.Group("/users")
 
 	routes.DELETE("/:username/followings/:followingusername", presenter.UserPresentation.DeleteUserFollowing, middleware.IsAuth())
+	routes.DELETE("/:username/bookmarks/:articleid", presenter.BookmarkPresentation.DeleteBookmark, middleware.IsAuth())
 	routes.DELETE("/:username", presenter.UserPresentation.DeleteUser, middleware.IsAuth())
 
 	routes.POST("/:username/followings/:followingusername", presenter.UserPresentation.PostUserFollowing, middleware.IsAuth())
+	routes.POST("/:username/bookmarks/:articleid", presenter.BookmarkPresentation.PostBookmark, middleware.IsAuth())
 
 	routes.PUT("/:username", presenter.UserPresentation.PutEditUser, middleware.IsAuth())
 
 	routes.GET("/:username/articles", presenter.ArticlePresentation.GetUserArticles)
+	routes.GET("/:username/bookmarks", presenter.BookmarkPresentation.GetUserBookmarks, middleware.IsAuth())
 	routes.GET("/:username/followers", presenter.UserPresentation.GetUserFollowers)
 	routes.GET("/:username/followings", presenter.UserPresentation.GetUserFollowings)
 	routes.GET("/:username", presenter.UserPresentation.GetDetailUser)
