79fbdd17963685c49782d2dda27a1d1238868825
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-28 13:30:37 +0700

    update: use env for host & port and use default logger

--- config/database.go
+++ config/database.go
@@ -2,9 +2,6 @@ package config
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,20 +12,16 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		ENV.DB_USERNAME,
 		ENV.DB_PASSWORD,
+		ENV.DB_HOST,
+		ENV.DB_PORT,
 		ENV.DB_NAME,
 	)
 
-	loggerWriter := log.New(os.Stdout, "\r\n", log.LstdFlags)
-	loggerConfig := logger.Config{
-		SlowThreshold: time.Second,
-		LogLevel:      logger.Silent,
-		Colorful:      true,
-	}
-
-	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.New(loggerWriter, loggerConfig)})
+	dbLog := logger.Default.LogMode(logger.Info)
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: dbLog})
 	if err != nil {
 		panic(err)
 	}
