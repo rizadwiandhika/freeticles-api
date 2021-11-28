89123b564f2b6aba8999d30051943365a2553db4
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-28 13:20:48 +0700

    add: follower handler methods

--- features/users/presentation/presentation.go
+++ features/users/presentation/presentation.go
@@ -12,7 +12,6 @@ import (
 
 type any interface{}
 type json map[string]any
-type list []any
 
 type UserPresentation struct {
 	userBusiness users.IBusiness
@@ -116,4 +115,96 @@ func (up *UserPresentation) DeleteUser(c echo.Context) error {
 	}
 
 	return c.JSON(status, json{"message": "Delete user success"})
+}
+
+func (up *UserPresentation) GetUserFollowers(c echo.Context) error {
+	var username string
+	echo.PathParamsBinder(c).String("username", &username)
+
+	user, err, status := up.userBusiness.FindUserFollowers(username)
+	if err != nil {
+		return c.JSON(status, json{
+			"message": "Failed retrieving followers",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(status, json{
+		"message":   "Success retrieving followers",
+		"username":  username,
+		"followers": response.FromSliceFollowerCore(user.Followers),
+	})
+}
+
+func (up *UserPresentation) GetUserFollowings(c echo.Context) error {
+	var username string
+	echo.PathParamsBinder(c).String("username", &username)
+
+	user, err, status := up.userBusiness.FindUserFollowings(username)
+	if err != nil {
+		return c.JSON(status, json{
+			"message": "Failed retrieving followers",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(status, json{
+		"message":   "Success retrieving followers",
+		"username":  username,
+		"followers": response.FromSliceFollowerCore(user.Followers),
+	})
+}
+
+func (up *UserPresentation) PostUserFollowing(c echo.Context) error {
+	var username string
+	var followingsUsername string
+
+	echo.PathParamsBinder(c).String("username", &username)
+	echo.PathParamsBinder(c).String("followingusername", &followingsUsername)
+
+	issuer := c.Get("user").(jwt.MapClaims)
+
+	if issuer["username"] != username && issuer["role"] != "admin" {
+		return c.JSON(http.StatusForbidden, json{
+			"message": "Unauthorized user!",
+		})
+	}
+
+	err, status := up.userBusiness.CreateFollower(followingsUsername, username)
+	if err != nil {
+		return c.JSON(status, json{
+			"message": "Failed following user",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(status, json{
+		"message": "Success following user",
+	})
+}
+
+func (up *UserPresentation) DeleteUserFollowing(c echo.Context) error {
+	var username string
+	var followingUsername string
+
+	echo.PathParamsBinder(c).String("username", &username)
+	echo.PathParamsBinder(c).String("followingusername", &followingUsername)
+
+	issuer := c.Get("user").(jwt.MapClaims)
+
+	if issuer["username"] != username && issuer["role"] != "admin" {
+		return c.JSON(http.StatusForbidden, json{
+			"message": "Unauthorized user!",
+		})
+	}
+
+	err, status := up.userBusiness.RemoveFollowing(username, followingUsername)
+	if err != nil {
+		return c.JSON(status, json{
+			"message": "Failed unfollowing user",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(status, json{"message": "Success unfollowing user"})
 }
