10829a4a9abcb7cd674a91434645cfea1eec39c0
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-30 20:22:20 +0700

    add: dto response

--- features/reactions/presentation/response/dto.go
+++ features/reactions/presentation/response/dto.go
@@ -1,5 +1,43 @@
 package response
 
+import (
+	"time"
+
+	"github.com/rizadwiandhika/miniproject-backend-alterra/features/reactions"
+)
+
 type LikeResponse struct {
 	Message string `json:"message"`
+}
+
+type Comment struct {
+	Commentar string    `json:"commentar"`
+	User      User      `json:"user"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type User struct {
+	ID       uint   `json:"userId"`
+	Username string `json:"username"`
+	Name     string `json:"name"`
+}
+
+func FromCommentCore(c *reactions.CommentCore) Comment {
+	return Comment{
+		Commentar: c.Commentar,
+		CreatedAt: c.CreatedAt,
+		User: User{
+			ID:       c.User.ID,
+			Username: c.User.Username,
+			Name:     c.User.Name,
+		},
+	}
+}
+
+func FromSliceCommentCore(c []reactions.CommentCore) []Comment {
+	var comments []Comment
+	for _, v := range c {
+		comments = append(comments, FromCommentCore(&v))
+	}
+	return comments
 }
