7f72cee191ab55ae26d565c2ddeca07497f04212
rizadwiandhika <rizadwiandhika@gmail.com>
2021-11-30 20:39:31 +0700

    update: setup articles translate, post, edit, & get comments

--- routes/article.go
+++ routes/article.go
@@ -7,19 +7,21 @@ import (
 )
 
 func SetupArticleRoutes(e *echo.Echo, presenter *factory.Presenter) {
+	e.GET("/:lang/articles/:id", presenter.ArticlePresentation.GetTranslatedDetailArticle)
+
 	routes := e.Group("/articles")
 
 	routes.DELETE("/:id/likes", presenter.ReactionPresentation.DeleteLike, middleware.IsAuth())
-	routes.DELETE("/:id", presenter.ArticlePresentation.DeleteArticle)
+	routes.DELETE("/:id", presenter.ArticlePresentation.DeleteArticle, middleware.IsAuth())
 
-	routes.POST(
-		"/:id/likes",
-		presenter.ReactionPresentation.PostLike,
-		middleware.IsAuth(),
-	)
+	routes.PUT("", presenter.ArticlePresentation.PutEditArticle, middleware.IsAuth())
+
+	routes.POST("/:id/likes", presenter.ReactionPresentation.PostLike, middleware.IsAuth())
 	routes.POST("/:id/comments", presenter.ReactionPresentation.PostComment, middleware.IsAuth())
 	routes.POST("/:id/reports", presenter.ReactionPresentation.PostReport, middleware.IsAuth())
+	routes.POST("", presenter.ArticlePresentation.PostArticle, middleware.IsAuth())
 
+	routes.GET("/:id/comments", presenter.ReactionPresentation.GetArticleComments)
 	routes.GET("/:id", presenter.ArticlePresentation.GetDetailArticle)
 	routes.GET("", presenter.ArticlePresentation.GetArticles)
 }
