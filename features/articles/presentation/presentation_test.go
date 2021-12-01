325631c12d1d208de920140b2e3f8d0f05b73c9e
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-01 15:55:05 +0700

    test: delete article

--- features/articles/presentation/presentation_test.go
+++ features/articles/presentation/presentation_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/rizadwiandhika/miniproject-backend-alterra/features/articles"
 	"github.com/rizadwiandhika/miniproject-backend-alterra/features/articles/presentation"
@@ -237,6 +238,140 @@ func TestGetTranslatedDetailArticle(t *testing.T) {
 		mc.Once()
 
 		if assert.NoError(t, articlePresentation.GetTranslatedDetailArticle(c)) {
+			assert.Equal(t, http.StatusInternalServerError, rec.Code)
+			assert.NotEqual(t, "", rec.Body.String())
+		}
+	})
+
+}
+
+func TestDeleteArticle(t *testing.T) {
+	mockHttpRequest := func() (*httptest.ResponseRecorder, echo.Context) {
+		req, _ := http.NewRequest(http.MethodDelete, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		return rec, c
+	}
+
+	t.Run("valid - DeleteArticle", func(t *testing.T) {
+		rec, c := mockHttpRequest()
+		defer rec.Result().Body.Close()
+
+		c.Set("user", jwt.MapClaims{
+			"userId":   1.0,
+			"username": "riza.dwi",
+			"role":     "admin",
+		})
+		c.SetPath("/articles/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("1")
+
+		var mc *mock.Call
+
+		mc = articleBusiness.On("FindArticleById", mock.AnythingOfType("uint"))
+		mc = mc.Return(articleValue, nil, 200)
+		mc.Once()
+
+		mc = articleBusiness.On("RemoveArticleById", mock.AnythingOfType("uint"))
+		mc = mc.Return(nil, 200)
+		mc.Once()
+
+		if assert.NoError(t, articlePresentation.DeleteArticle(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.NotEqual(t, "", rec.Body.String())
+		}
+	})
+
+	t.Run("valid - error when articleId < 1", func(t *testing.T) {
+		rec, c := mockHttpRequest()
+		defer rec.Result().Body.Close()
+
+		c.SetPath("/articles/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("0")
+
+		if assert.NoError(t, articlePresentation.DeleteArticle(c)) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
+			assert.NotEqual(t, "", rec.Body.String())
+		}
+	})
+
+	t.Run("valid - error when article not found", func(t *testing.T) {
+		rec, c := mockHttpRequest()
+		defer rec.Result().Body.Close()
+
+		c.Set("user", jwt.MapClaims{
+			"userId":   1.0,
+			"username": "riza.dwi",
+			"role":     "admin",
+		})
+		c.SetPath("/articles/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("1")
+
+		var mc *mock.Call
+
+		mc = articleBusiness.On("FindArticleById", mock.AnythingOfType("uint"))
+		mc = mc.Return(articleValue, errors.New("abc"), 123)
+		mc.Once()
+
+		if assert.NoError(t, articlePresentation.DeleteArticle(c)) {
+			assert.Equal(t, http.StatusInternalServerError, rec.Code)
+			assert.NotEqual(t, "", rec.Body.String())
+		}
+	})
+
+	t.Run("valid - error when unauthorized user delete an artcle", func(t *testing.T) {
+		rec, c := mockHttpRequest()
+		defer rec.Result().Body.Close()
+
+		c.Set("user", jwt.MapClaims{
+			"userId":   2.0,
+			"username": "hernowo",
+			"role":     "user",
+		})
+		c.SetPath("/articles/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("1")
+
+		var mc *mock.Call
+
+		mc = articleBusiness.On("FindArticleById", mock.AnythingOfType("uint"))
+		mc = mc.Return(articleValue, nil, 200)
+		mc.Once()
+
+		if assert.NoError(t, articlePresentation.DeleteArticle(c)) {
+			assert.Equal(t, http.StatusForbidden, rec.Code)
+			assert.NotEqual(t, "", rec.Body.String())
+		}
+	})
+
+	t.Run("valid - error when removing article", func(t *testing.T) {
+		rec, c := mockHttpRequest()
+		defer rec.Result().Body.Close()
+
+		c.Set("user", jwt.MapClaims{
+			"userId":   1.0,
+			"username": "riza.dwi",
+			"role":     "admin",
+		})
+		c.SetPath("/articles/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("1")
+
+		var mc *mock.Call
+
+		mc = articleBusiness.On("FindArticleById", mock.AnythingOfType("uint"))
+		mc = mc.Return(articleValue, nil, 200)
+		mc.Once()
+
+		mc = articleBusiness.On("RemoveArticleById", mock.AnythingOfType("uint"))
+		mc = mc.Return(errors.New("abc"), 500)
+		mc.Once()
+
+		if assert.NoError(t, articlePresentation.DeleteArticle(c)) {
 			assert.Equal(t, http.StatusInternalServerError, rec.Code)
 			assert.NotEqual(t, "", rec.Body.String())
 		}
